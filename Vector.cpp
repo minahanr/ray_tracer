3114e673ba40e018426ada66d4e25c498d70b6ae
Robert Minahan <minahanr75@gmail.com>
2020-10-24 03:28:01 -0400

    Vector class

---
+++ Vector.cpp
@@ -0,0 +1,13 @@
+class Vector {
+    public:
+        Vector(double x, double y, double z) {
+            this->x = x;
+            this->y = y;
+            this->z = z;
+        }
+
+    private:
+        double x;
+        double y;
+        double z;
+}
