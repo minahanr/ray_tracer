153e7401d1d8e131377f7ab35dcd27e5bee6ef22
Robert Minahan <minahanr75@gmail.com>
2020-10-25 00:53:01 -0400

    utilities

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include "Ray.h"
+#include "Hittable.h"
+#include "Sphere.h"
+
 #include<iostream>
 #include<vector>
 
-#include "Ray.h"
-#include "Hittable.h"
+
 
 int main() {
     std::vector<Hittable> hittable_list;
 
-    
+    hittable_list.push_back(Sphere())
 }
