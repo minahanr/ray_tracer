ff5de1c4509ad560b7c45d041ff9b27b3f7c942c
Robert Minahan <minahanr75@gmail.com>
2020-10-24 23:44:20 -0400

    typo

--- Vector3d.h
+++ Vector3d.h
@@ -5,6 +5,7 @@ class Vector3d {
         double getX();
         double getY();
         double getZ();
+        double magnitude();
 
         Vector3d operator+(const Vector3d& vec);
         Vector3d operator*(double scalar);
