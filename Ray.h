595930978462a98811d4f3475319b83bc5823e64
Robert Minahan <minahanr75@gmail.com>
2020-10-24 23:42:35 -0400

    Deleted excess function in header

--- Ray.h
+++ Ray.h
@@ -3,10 +3,9 @@
 class Ray {
     public:
         Ray(Vector3d &origin, Vector3d &direction, Vector3d &image_point);
-        Vector3d find_intersection(double time_start, double time_end, double delta_t, double delta_r);
-        Vector3d* getOrigin();
-        Vector3d* getDirection();
-        Vector3d* getImage_point();
+        Vector3d* getOrigin() const;
+        Vector3d* getDirection() const;
+        Vector3d* getImage_point() const;
         
     private:
         Vector3d* origin;
