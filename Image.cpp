97d9ee7ba78967035ab8d664c8de05b511ea2cf1
Robert Minahan <minahanr75@gmail.com>
2020-10-24 23:02:12 -0400

    Added attributes to Image class

--- Image.cpp
+++ Image.cpp
@@ -3,12 +3,20 @@
 
 class Image {
     public:
-        Image(Vector3d &lower_left_corner, Vector3d &upper_right_corner) {
+        Image(Vector3d &lower_left_corner, Vector3d &lower_right_corner, Vector3d &upper_left_corner, Vector3d &upper_right_corner, double horizontal_res, double vertical_res) {
             this->lower_left_corner = &lower_left_corner;
+            this->lower_right_corner = &lower_right_corner;
+            this->upper_left_corner = &upper_left_corner;
             this->upper_right_corner = &upper_right_corner;
+            this->horizontal_res = horizontal_res;
+            this->vertical_res = vertical_res;
         }
 
     private:
         Vector3d* lower_left_corner;
+        Vector3d* lower_right_corner;
         Vector3d* upper_right_corner;
+        Vector3d* upper_left_corner;
+        double horizontal_res;
+        double vertical_res;
 };
--- Image.h
+++ Image.h
@@ -2,9 +2,13 @@
 
 class Image {
     public:
-        Image(Vector3d &lower_left_corner, Vector3d &upper_right_corner);
+        Image(Vector3d &lower_left_corner, Vector3d &lower_right_corner, Vector3d &upper_left_corner, Vector3d &upper_right_corner, double horizontal_res, double vertical_res);
 
     private:
         Vector3d* lower_left_corner;
+        Vector3d* lower_right_corner;
         Vector3d* upper_right_corner;
+        Vector3d* upper_left_corner;
+        double horizontal_res;
+        double vertical_res;
 };
