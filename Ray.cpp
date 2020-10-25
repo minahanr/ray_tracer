f25fecc37bb7f9642cd689b58249c9bea909996f
Robert Minahan <minahanr75@gmail.com>
2020-10-24 23:03:06 -0400

    Added new attribute for image class

--- Ray.cpp
+++ Ray.cpp
@@ -2,9 +2,10 @@
 
 class Ray {
     public:
-        Ray(Vector3d &origin, Vector3d &direction) {
+        Ray(Vector3d &origin, Vector3d &direction, Vector3d &image_point) {
             this->origin = &origin;
             this->direction = &direction;
+            this->image_point = &image_point;
         }
 
         Vector3d* getOrigin() {
@@ -14,7 +15,13 @@ class Ray {
         Vector3d* getDirection() {
             return direction;
         }
+
+        Vector3d* getImage_point() {
+            return image_point;
+        }
+        
     private:
         Vector3d* origin;
         Vector3d* direction;
+        Vector3d* image_point;
 };
--- Ray.h
+++ Ray.h
@@ -2,12 +2,14 @@
 
 class Ray {
     public:
-        Ray(Vector3d &origin, Vector3d &direction);
+        Ray(Vector3d &origin, Vector3d &direction, Vector3d &image_point);
         Vector3d find_intersection(double time_start, double time_end, double delta_t, double delta_r);
         Vector3d* getOrigin();
         Vector3d* getDirection();
+        Vector3d* getImage_point();
         
     private:
         Vector3d* origin;
         Vector3d* direction;
+        Vector3d* image_point;
 };
