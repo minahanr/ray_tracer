5e838d6c1f79af85f52794bceae65a81f420e5f3
Robert Minahan <minahanr75@gmail.com>
2020-10-24 21:44:43 -0400

    Added operator overloading and getter functions to Vector3d

--- Vector3d.cpp
+++ Vector3d.cpp
@@ -2,14 +2,43 @@
 
 class Vector3d {
     public:
+        Vector3d() {
+            Vector3d(0, 0, 0);
+        }
+        
         Vector3d(double x, double y, double z) {
             this->x = x;
             this->y = y;
             this->z = z;
         }
 
+        double getX() const {
+            return this->x;
+        }
+
+        double getY() const {
+            return this->y;
+        }
+
+        double getZ() const {
+            return this->z;
+        }
+
+        Vector3d operator+(const Vector3d& vec) {
+            return Vector3d(this->x + vec.getX(), this->y + vec.getY(), this->z + vec.getZ());
+        }
+        Vector3d operator*(double scalar) {
+            return Vector3d(this->x * scalar, this->y * scalar, this->z * scalar);
+        }
+
     private:
         double x;
         double y;
         double z;
+
+
 };
+
+Vector3d operator*(double scalar, Vector3d& vec) {
+    return vec * scalar;
+}
--- Vector3d.h
+++ Vector3d.h
@@ -1,7 +1,13 @@
 class Vector3d {
     public:
+        Vector3d();
         Vector3d(double x, double y, double z);
+        double getX();
+        double getY();
+        double getZ();
 
+        Vector3d operator+(const Vector3d& vec);
+        Vector3d operator*(double scalar);
     private:
         double x;
         double y;
