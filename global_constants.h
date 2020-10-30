852a942593c389dad6d16daca3ce74423cedabc2
Robert Minahan <minahanr75@gmail.com>
2020-10-29 23:15:20 -0400

    Changed constants

--- global_constants.h
+++ global_constants.h
@@ -1,5 +1,5 @@
 const double START_TIME = 0;
-const double END_TIME = 100;
+const double END_TIME = 20;
 const double DELTA_TIME = 0.01;
 const double APPROX_EQUIV = 0.01;
 const double RESOLUTION = 0.01;
