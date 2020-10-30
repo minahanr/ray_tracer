35dc928db43e0381c4da856deeffb7927da04dbe
Robert Minahan <minahanr75@gmail.com>
2020-10-30 15:59:22 -0400

    Changed Ray structure

--- Ray.cpp
+++ Ray.cpp
@@ -1,19 +1,19 @@
 #include "Ray.h"
 
 Ray::Ray(Vector3d origin, Vector3d direction, Vector3d image_point) {
-    this->origin = &origin;
-    this->direction = &direction;
-    this->image_point = &image_point;
+    this->origin = origin;
+    this->direction = direction;
+    this->image_point = image_point;
 }
 
-Vector3d* Ray::getOrigin() {
+Vector3d Ray::getOrigin() {
     return origin;
 }
 
-Vector3d* Ray::getDirection() {
+Vector3d Ray::getDirection() {
     return direction;
 }
 
-Vector3d* Ray::getImage_point() {
+Vector3d Ray::getImage_point() {
     return image_point;
 }
--- Ray.h
+++ Ray.h
@@ -6,14 +6,14 @@
 class Ray {
     public:
         Ray(Vector3d origin, Vector3d direction, Vector3d image_point);
-        Vector3d* getOrigin();
-        Vector3d* getDirection();
-        Vector3d* getImage_point();
+        Vector3d getOrigin();
+        Vector3d getDirection();
+        Vector3d getImage_point();
         
     private:
-        Vector3d* origin;
-        Vector3d* direction;
-        Vector3d* image_point;
+        Vector3d origin;
+        Vector3d direction;
+        Vector3d image_point;
 };
 
 #endif
