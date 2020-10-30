be8b6ea21dcc0f08f53aa21a2bc51e02971d7ae8
Robert Minahan <minahanr75@gmail.com>
2020-10-30 15:58:51 -0400

    Updated intersection method

--- main.cpp
+++ main.cpp
@@ -10,14 +10,54 @@
 #include<iostream>
 #include<vector>
 #include<list>
+#include<time.h>
 
 
-
-void print_color(double r, double g, double b) {
-    int ir = static_cast<int>(255.999 * r);
-    int ig = static_cast<int>(255.999 * g);
-    int ib = static_cast<int>(255.999 * b);
+void print_color(Vector3d color) {
+    int ir = static_cast<int>(255.999 * color.getX());
+    int ig = static_cast<int>(255.999 * color.getY());
+    int ib = static_cast<int>(255.999 * color.getZ());
     std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+}
+
+Vector3d randInUnitSphere() {
+    double x = ((double) rand()) / (RAND_MAX + 1.0);
+    double y = ((double) rand()) / (RAND_MAX + 1.0);
+    double z = ((double) rand()) / (RAND_MAX + 1.0);
+    Vector3d vec(x, y, z);
+    if (vec * vec <= 1) {
+        return vec;
+    } else {
+        return randInUnitSphere();
+    }
+}
+
+Vector3d intersects(Ray ray, std::vector<Sphere> surface_list, int depth) {
+
+    if (depth <= 0) {
+        return Vector3d(0, 0, 0);
+    }
+
+    std::vector<Sphere>::iterator surface_it = surface_list.begin();
+    Vector3d intersectionPoint, dif, normal, target; double distance, r_squared;
+
+    for (double time = DELTA_TIME; time < END_TIME; time += DELTA_TIME) {
+        intersectionPoint = ray.getOrigin() + ray.getDirection() * time;
+        for (surface_it = surface_list.begin(); surface_it != surface_list.end(); surface_it++) {
+            dif = intersectionPoint - surface_it->getCenter();
+            distance = dif * dif;
+            r_squared = surface_it->getRadius() * surface_it->getRadius();
+            if (distance - r_squared <= APPROX_EQUIV && distance - r_squared >= -1 * APPROX_EQUIV) {
+                normal = Unit_vector(intersectionPoint - surface_it->getCenter());
+                target = intersectionPoint + normal + randInUnitSphere();
+                Ray newRay = Ray(intersectionPoint, Unit_vector(target - intersectionPoint), ray.getImage_point());
+                return intersects(newRay, surface_list, depth - 1) * 0.5;
+            }
+        }
+    }
+    
+    double t = 0.5 * (ray.getDirection().getY() + 1.0);
+    return Vector3d(1, 1, 1) * (1.0 - t) + Vector3d(0.5, 0.7, 1.0) * t;
 }
 
 int main() {
@@ -30,28 +70,12 @@ int main() {
     Vector3d image_point;
     bool skip = true;
 
-    for (int i = 0; i < image.getHeight(); i++) {
+    for (int i = image.getHeight() - 1; i >= 0; i--) {
         for (int j = 0; j < image.getWidth(); j++) {
-            skip = false;
             image_point = image.getCorner() + image.getVectorDirections()[0] * j * image.getHorizontalRes() + image.getVectorDirections()[1] * i * image.getVerticalRes();
             ray = Ray(camera.getPoint(), Unit_vector(image_point - camera.getPoint()), image_point);
-            for (double time = START_TIME; time < END_TIME; time += DELTA_TIME) {
-                for (std::vector<Sphere>::iterator surface_it = surface_list.begin(); surface_it != surface_list.end(); surface_it++) {
-                    if (time && surface_it->intersects(ray, time) != *(ray.getOrigin())) {
-                        skip = true;
-                        print_color(1, 0, 0);
-                        break;
-                    }
-                }
 
-                if (skip) {
-                    break;
-                }
-            }
-
-            if (!skip) {
-                print_color(0, 0, 0);
-            }
+            print_color(intersects(ray, surface_list, 20));
         }
     }
 }
